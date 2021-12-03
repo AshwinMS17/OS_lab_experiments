efdab453da346b659955f2be47856d11ec045baf
AshwinMS17 <77296355+AshwinMS17@users.noreply.github.com>
2021-12-03 12:31:05 +0530

    Add files via upload

--- pagereplfu.c
+++ pagereplfu.c
@@ -14,50 +14,38 @@ int search(int ar[],int size,int s)
 	return n;
 }
 
-int compare(int fr[],int frames,int pages[],int index,int fr1,int fr2)
+int compare(int frames,int fr1,int fr2,int count[])
 {
-    int ifr1=-1,ifr2=-1;
-    for(int i=index-1;i>=0;i--)
-    {
-        if(ifr1==-1&&fr[fr1]==pages[i])
-        {
-            ifr1=i;
-        }
-        if(ifr2==-1&&fr[fr2]==pages[i])
-        {
-            ifr2=i;
-        }
-    }
-    if(ifr1<ifr2)
-    {
-        return ifr1;
-    }
-    return ifr2;
+	if(count[fr1]<count[fr2])
+	{
+		return fr1;
+	}
+	return fr2;
 }
 
-int framefinder(int fr[],int frames,int pages[],int index)
+int framefinder(int fr[],int frames,int pages[],int index,int count[])
 {
 	int find_frame=frames,find_page=index;
 	//printf("\nfind is %d\n",find);
 	for(int i=0;i<frames;i++)
 	{
 		int c=0;
-        for(int j=index-1;j>=0;j--)
+        	for(int j=index-1;j>=0;j--)
 		{
 			if(fr[i]==pages[j])//removing find >j from if condition
 			{
 				c++;
 			}
 		}
-        if(c<find_page)
-        {
-            find_page=c;
-            find_frame=i;
-        }
-        else if(c==find_page)
-        {
-            find_frame=compare(fr,frames,pages,index,i,find_frame);//lr used of the two
-        }	
+        	if(c<find_page)
+        	{
+            		find_page=c;
+            		find_frame=i;
+        	}
+        	else if(c==find_page)
+        	{
+            		find_frame=compare(frames,i,find_frame,count);//lr used of the two
+        	}	
 	}
 	//printf("\nfound at %d\n",find);
 	return find_frame;
@@ -66,7 +54,7 @@ int framefinder(int fr[],int frames,int pages[],int index)
 void main()
 {
 	int pages[20];
-	int size,frames,empty=0,miss=0,hit=0,fr[10],find;
+	int size,frames,empty=0,miss=0,hit=0,fr[10],find,count[10],c=0,disp=0;
 	printf("Enter no:of pages:");
 	scanf("%d",&size);
 	printf("Enter page string:");
@@ -80,38 +68,50 @@ void main()
 	for(int i=0;i<frames;i++)
 	{
 		fr[i]=-1;
+		count[i]=-1;
 	}
+	printf("\nFrames will be as follows:\n\n");
 	for(int i=0;i<size;i++)
 	{
+		disp=0;
 		if(search(fr,frames,pages[i])==0)
 		{
 			if(empty<frames)
 			{
 				fr[empty]=pages[i];
+				count[empty]=c++;
 				empty++;
+				
 			}
 			else
 			{
-				find=framefinder(fr,frames,pages,i);
+				find=framefinder(fr,frames,pages,i,count);
 				fr[find]=pages[i];
+				count[find]=c++;
+				
 			}
 			miss++;	
+			disp=1;
 		}
 		else
 		{
 			hit++;
 		}
-		//printf("%d-- ",i);
+		
 		for(int j=0;j<frames;j++)
 		{
 			if(fr[j]==-1)
 			{
-				printf("--  ");
+				printf("-  ");
 			}
 			else
 			{
 				printf("%d  ",fr[j]);
 			}
+		}
+		if(disp==1)
+		{
+			printf("Page Fault %d.",miss);
 		}
 		printf("\n");
 	}
